4e3d851821cac028262ab02c77b9cbfc6edb8a40
anish <rpaudel379@gmail.com>
2021-01-17 15:21:00 +0530

    added responsive and removed useEffect from Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext, useEffect } from "react";
+import React, { useState, useContext } from "react";
 import UserContext from "../context/UserContext";
 import { Link } from "react-router-dom";
 import cookie from "js-cookie";
